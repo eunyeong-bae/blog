6e9a444142b580931ff07529d4e8420b1c78ca97
배은영 <adcadcad@naver.com>
2023-07-18 14:33:32 +0900

    'test'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>HELLO THIS IS MY RECENT PROJECT!</p>
+    </div>
+  );
+}
+
+export default App;
